64a16ed2d6c999786560aef6cccad81d9c6e1c0e
Rustem Islamuratov <rustem.islamuratov@gmail.com>
2019-05-24 14:45:43 +0500

    Add react hook example - useRef

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# reactHooksExamples
+# react-hooks-examples
+
+### How to run
+
+1) open https://codesandbox.io/embed/test-xribn
+2) copy any example code to index.js
+3)
+ enjoy!
---
+++ src/3-useRef/index.js
@@ -0,0 +1,26 @@
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const App = () => {
+  const [text, setText] = useState("frog");
+  const spanRef = useRef();
+  const handleChange = e => {
+    setText(e.target.value);
+    const spanNode = spanRef.current;
+    spanNode.textContent = e.target.value + " - " + e.target.value;
+  };
+
+  return (
+    <div className="App">
+      <h2>Input:</h2>
+      <input value={text} onChange={handleChange} />
+      <h2>Value:</h2>
+      {text}
+      <br />
+      <span ref={spanRef} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
